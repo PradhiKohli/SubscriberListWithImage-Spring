3c49b1d05a4e3e2ef71a11397abfebb5f3c45dca
Pradhi Kohli <68760290+PradhiKohli@users.noreply.github.com>
2021-02-15 22:43:05 -0600

    First Commit

---
+++ SubscriberListImage/src/main/java/com/tts/subscriberlist/SubscriberListApplication.java
@@ -0,0 +1,13 @@
+package com.tts.subscriberlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriberListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriberListApplication.class, args);
+	}
+
+}
